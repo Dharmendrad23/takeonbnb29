5a9d33b13de0b60eb69c071a2d794e6f996fffb7
Dharmendrad23 <dharmendrashah1439@gmail.com>
2026-07-25 16:15:07 +0530

    node --version

--- apps/pocketbase/pb_migrations/1779965684_001_delete_bookings_by_filter.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../pb_data/types.d.ts" />
-migrate((app) => {
-  const records = app.findRecordsByFilter("bookings", "id='0t8ma5ivl3fg0tl'");
-  for (const record of records) {
-    app.delete(record);
-  }
-}, (app) => {
-  // Rollback: record data not stored, manual restore needed
-})
